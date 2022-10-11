25eff7f9ad9dddbe49635b35a5d76d43bed0e934
empschool <115432690+empschool@users.noreply.github.com>
2022-10-11 18:46:43 -0300

    Commit from TeleportHQ

--- pages/local.js
+++ pages/local.js
@@ -22,11 +22,11 @@ const Local = (props) => {
             content="Serviço de marketing completo para pequenos negócios, gestor de trafego profissional, social media profissional, desenvolvimento de plano de negócios "
           />
         </Head>
-        <div>
+        <div className="local-html-node">
           <span
             dangerouslySetInnerHTML={{
               __html:
-                '<iframe\r\n    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.63021247021!2d-46.592954500000005!3d-23.545798999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xda743ac39c6f5538!2sStartpee!5e0!3m2!1spt-BR!2sbr!4v1665524402712!5m2!1spt-BR!2sbr"\r\n    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"\r\n    referrerpolicy="no-referrer-when-downgrade"></iframe>',
+                '<div style="padding-bottom: 56.25%; position: relative; height: 0; overflow: hidden; max-width: 100%;"><iframe\r\n        src="https://player.adventr.io/index.html?link=https%3A%2F%2Fd252srr1zuysk4.cloudfront.net%2Fclients%2F7804%2F9437%2Fpublished%2F7804-portfolio-29993165.data&gdpr=skip"\r\n        frameborder="0" scrolling="no" allowfullscreen\r\n        allow="autoplay; fullscreen; clipboard-read; clipboard-write; encrypted-media; geolocation; microphone"\r\n        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>',
             }}
           />
         </div>
@@ -41,6 +41,15 @@ const Local = (props) => {
             align-items: center;
             flex-direction: column;
             justify-content: center;
+          }
+          .local-html-node {
+            top: 230px;
+            left: 0px;
+            right: 0px;
+            width: 517px;
+            height: 466px;
+            margin: auto;
+            position: absolute;
           }
         `}
       </style>
